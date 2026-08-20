84d444c5b729fae2f8f1c480a3966b74b0d4468e
Link4Electronics <link4electronics@gmail.com>
2026-08-20 12:04:15 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,24 +7,20 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    libdecor        \
     openttd         \
     openttd-opengfx \
-    openttd-opensfx
+    openttd-opensfx \
+    pipewire-audio  \
+    pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-make-aur-package openttd-openmsx
+echo "Getting OpenTTD-OpenMSX..."
+echo "---------------------------------------------------------------"
+VERSION=0.4.2
+wget https://cdn.openttd.org/openmsx-releases/${VERSION}/openmsx-${VERSION}-all.zip
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/share/openttd/gm
+bsdtar -xOf openmsx-${VERSION}-all.zip openmsx-${VERSION}.tar | bsdtar -xvf - --include='openmsx-0.4.2/*.mid' --include='openmsx-0.4.2/openmsx.*' -C ./AppDir/share/openttd/gm --strip-components=1
