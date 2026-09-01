a0ffa5d10c9f5d65b61a52219d5c0492dba44238
Link4Electronics <link4electronics@gmail.com>
2026-09-01 19:28:53 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -29,4 +29,4 @@ VERSION=0.4.2
 https://cdn.openttd.org/openmsx-releases/${VERSION}/openmsx-${VERSION}-all.zip
 
 mkdir -p ./AppDir/share/openttd/gm
-bsdtar -xOf openmsx-${VERSION}-all.zip openmsx-0.4.2.tar | bsdtar -xvf - --include='openmsx-0.4.2/*.mid' -C ./AppDir/share/openttd/gm --strip-components=1
+bsdtar -xOf openmsx-${VERSION}-all.zip openmsx-${VERSION}.tar | bsdtar -xvf - --include='openmsx-${VERSION}/*.mid' --include='openmsx-${VERSION}/openmsx.*' -C ./AppDir/share/openttd/gm --strip-components=1
