1d1ce631252d4652c70780b5f423a4915fa6c794
Link4Electronics <link4electronics@gmail.com>
2026-09-01 19:56:34 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,12 +17,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package openttd-openmsx
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
 echo "Getting OpenTTD-OpenMSX..."
 echo "---------------------------------------------------------------"
 VERSION=0.4.2
