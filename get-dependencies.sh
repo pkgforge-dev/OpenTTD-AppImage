3000d23c9197300070463e6e55a6e883f29802cc
Link4Electronics <link4electronics@gmail.com>
2026-09-01 19:20:22 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,11 +6,12 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    openttd         \
-    openttd-opengfx \
-    openttd-opensfx \
-    pipewire-audio  \
+pacman -Syu --noconfirm   \
+    freepats-general-midi \
+    openttd               \
+    openttd-opengfx       \
+    openttd-opensfx       \
+    pipewire-audio        \
     pipewire-jack
 
 echo "Installing debloated packages..."
@@ -18,14 +19,14 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 # Comment this out if you need an AUR package
-make-aur-package openttd-openmsx
+#make-aur-package openttd-openmsx
 
 # If the application needs to be manually built that has to be done down here
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+echo "Getting OpenTTD-openmsx..."
+echo "---------------------------------------------------------------"
+VERSION=0.4.2
+https://cdn.openttd.org/openmsx-releases/${VERSION}/openmsx-${VERSION}-all.zip
+
+bsdtar -xvf openmsx-${VERSION}-all.zip
